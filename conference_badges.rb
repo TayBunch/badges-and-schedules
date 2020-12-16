a494c63d9a960bf3fa341a9b66b67fcf0f96989b
my.github.bunchtaylor30@gmail.com <my.github.bunchtaylor30@gmail.com>
2020-12-15 22:25:51 -0600

    commit tree ee3f2c75b17e6ee2822642717d521692bd1306b6

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    attendees.map do |attendee|
+        badge_maker(attendee)
+    end
+end
+
+def assign_rooms(attendees)
+
+    end
+end 
